36cdb34e1f5a00de417e3a736120b98f6c07148a
SharleneRyan <sa.ryan@live.com>
2019-04-05 10:41:46 +1300

    added icon to mana types

--- seeds/mana.js
+++ seeds/mana.js
@@ -5,12 +5,11 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('mana').insert([
-
-        {id: 1, type: 'White Mana',cost: '7-8'url:},
-        {id: 2, type: 'Red Mana', cost: '5-6'},
-        {id: 3, type: 'Green Mana', cost: '3-4'},
-        {id: 4, type: 'Black Mana', cost: '1-2'}
-
+        {id: 1, type: 'White Mana',cost: '7-8', url:'/public/img/White_Mana.png'},
+        {id: 2, type: 'Red Mana',  cost: '5-6', url:'/public/img/Red_Mana.png'},
+        {id: 3, type: 'Green Mana',cost: '3-4', url:'/public/img/Green_Mana.png'},
+        {id: 4, type: 'Black Mana',cost: '1-2', url:'/public/img/Black_Mana.png'},
+        {id: 5, type: 'Blue Mana', cost: '1-2', url:'/public/img/Blue_Mana.png'},
       ]);
     });
 };
