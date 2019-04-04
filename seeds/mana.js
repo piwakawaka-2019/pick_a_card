c0215dbd43b886a04693eae73da3af9e561b2fc0
SharleneRyan <sa.ryan@live.com>
2019-04-05 10:04:06 +1300

    Pictures to add for Mat

--- seeds/mana.js
+++ seeds/mana.js
@@ -6,7 +6,7 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('mana').insert([
 
-        {id: 1, type: 'White Mana',cost: '7-8'},
+        {id: 1, type: 'White Mana',cost: '7-8'url:},
         {id: 2, type: 'Red Mana', cost: '5-6'},
         {id: 3, type: 'Green Mana', cost: '3-4'},
         {id: 4, type: 'Black Mana', cost: '1-2'}
