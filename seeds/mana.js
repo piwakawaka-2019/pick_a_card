48a8df59b9dd1bfaaa9d6e88d4b188c02eac2da4
zozo-url <zozo-url@protonmail.ch>
2019-04-05 10:46:28 +1300

    basic functionality

--- seeds/mana.js
+++ seeds/mana.js
@@ -1,16 +1,16 @@
 
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
   return knex('mana').del()
     .then(function () {
       // Inserts seed entries
       return knex('mana').insert([
 
-        {id: 1, type: 'White Mana',cost: '7-8'},
-        {id: 2, type: 'Red Mana', cost: '5-6'},
-        {id: 3, type: 'Green Mana', cost: '3-4'},
-        {id: 4, type: 'Black Mana', cost: '1-2'}
-
+        { id: 1, type: 'White Mana', cost: '7-8' },
+        { id: 2, type: 'Red Mana', cost: '5-6' },
+        { id: 3, type: 'Green Mana', cost: '3-4' },
+        { id: 4, type: 'Black Mana', cost: '1-2' },
+        { id: 5, type: 'Blue Mana', cost: '1-2' },
       ]);
     });
 };
