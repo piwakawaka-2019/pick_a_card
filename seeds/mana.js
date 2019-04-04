01affc87fb4af4c676c13b57ade64327f2f7e8fc
SharleneRyan <sa.ryan@live.com>
2019-04-05 10:49:41 +1300

    saving

--- seeds/mana.js
+++ seeds/mana.js
@@ -9,7 +9,7 @@ exports.seed = function(knex, Promise) {
         {id: 2, type: 'Red Mana',  cost: '5-6', url:'/public/img/Red_Mana.png'},
         {id: 3, type: 'Green Mana',cost: '3-4', url:'/public/img/Green_Mana.png'},
         {id: 4, type: 'Black Mana',cost: '1-2', url:'/public/img/Black_Mana.png'},
-        {id: 5, type: 'Blue Mana', cost: '1-2', url:'/public/img/Blue_Mana.png'},
+        {id: 5, type: 'Blue Mana', cost: '1-2', url:'git /public/img/Blue_Mana.png'},
       ]);
     });
 };
